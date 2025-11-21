9bd428364c45065016d60d1bb92745249b150d1c
Vladyslav Nosal <vladyslavnosal@gmail.com>
2025-11-20 21:35:35 -0800

    fix: fix "vlad" command

---
+++ src/bot/commands/vlad.ts
@@ -0,0 +1,11 @@
+import { type Context } from 'telegraf';
+import { type Update } from 'telegraf/typings/core/types/typegram';
+
+import { type ICommand } from './types';
+
+export class VladCommand implements ICommand {
+  public execute = async (ctx: Context<Update>): Promise<void> => {
+    await ctx.reply('Ты вызвала секретную команду!');
+  };
+}
+
--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,19 +1,19 @@
 import { Telegraf } from 'telegraf';
 
 import { StartCommand } from './commands/start';
+import { VladCommand } from './commands/vlad';
 import { logMiddleware } from './middlwares/log';
-import { type Context } from 'telegraf';
 
 export function createBot(token: string) {
   const startCommand = new StartCommand();
+  const vladCommand = new VladCommand();
 
   const bot = new Telegraf(token);
 
   bot.use(logMiddleware);
 
   bot.start(startCommand.execute);
-
-  bot.on('vlad', (ctx: Context) => ctx.reply("Ты вызвал секретную команду!"));
+  bot.command('vlad', vladCommand.execute);
 
   return bot;
 }
